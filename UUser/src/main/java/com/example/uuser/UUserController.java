268508c155128d9a8ffbe0c421769d559af2da92
davidpatsina <dpats19@freeuni.edu.ge>
2024-06-06 18:28:17 +0400

    Add crud method to UUSer controler

--- UUser/src/main/java/com/example/uuser/UUserController.java
+++ UUser/src/main/java/com/example/uuser/UUserController.java
@@ -1,12 +1,19 @@
 package com.example.uuser;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/users")
 public class UUserController {
     @GetMapping
-    public String sayHello() {
+    public String getUser() {
         return "Hello, World!";
     }
+    @PostMapping
+    public String setUser(@RequestBody User user) {
+        return "Hello, World!";
+    }
+    @DeleteMapping String deleteUser(@RequestBody User user){return "Hello, World!";}
+    @PutMapping String updateUser(@RequestBody User user){return "Hello, World!";}
 }
