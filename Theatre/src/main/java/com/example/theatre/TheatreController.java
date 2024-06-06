ffe6bcddf5c054400d17dea9cae5244d655b20a4
davidpatsina <dpats19@freeuni.edu.ge>
2024-06-06 18:53:54 +0400

    Add CRUD method to Theatre module

--- Theatre/src/main/java/com/example/theatre/TheatreController.java
+++ Theatre/src/main/java/com/example/theatre/TheatreController.java
@@ -1,12 +1,19 @@
 package com.example.theatre;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/theatres")
 public class TheatreController {
     @GetMapping
-    public String sayHello() {
+    public String getTheatre() {
         return "Hello, World!";
     }
+    @PostMapping
+    public String setTheatre() {
+        return "Hello, World!";
+    }
+    @DeleteMapping
+    String deleteTheatre(){return "Hello, World!";}
+    @PutMapping String updateTheatre(){return "Hello, World!";}
 }
