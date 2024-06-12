a148df97a0109cbc6a4be10d7e5042e264bb5f1d
davidpatsina <dpats19@freeuni.edu.ge>
2024-06-12 20:30:55 +0400

    Theatre controler implementation

--- Theatre/src/main/java/com/example/theatre/api/controller/TheatreController.java
+++ Theatre/src/main/java/com/example/theatre/api/controller/TheatreController.java
@@ -1,12 +1,44 @@
 package com.example.theatre.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.theatre.api.dto.TheatreRequestDto;
+import com.example.theatre.service.TheatreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class TheatreController {
+    private final TheatreService theatreService;
+
     @GetMapping
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/theatre")
+    public List<TheatreRequestDto> getTheatres(){
+        return theatreService.getAll();
+    }
+
+    @GetMapping("/theatre/{theatreid}")
+    public TheatreRequestDto getById(@PathVariable("theatreid") int theatreid){
+        return theatreService.getTheatreById(theatreid);
+    }
+
+    @PostMapping("/theatre")
+    public TheatreRequestDto create(@RequestBody TheatreRequestDto theatreRequestDto){
+        return theatreService.addTheatre(theatreRequestDto);
+    }
+
+    @PutMapping("/theatre")
+    public TheatreRequestDto update(@RequestBody int id, TheatreRequestDto theatreRequestDto){
+        return theatreService.updateTheatre(id, theatreRequestDto);
+    }
+
+    @DeleteMapping("/theatre/{theatreid}")
+    public void delete(@PathVariable("theatreid") int theatreid){
+        theatreService.delete(theatreid);
     }
 }
