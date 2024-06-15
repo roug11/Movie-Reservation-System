12c3d9e197b44cfef733b4fcb16166b0453edea1
davidpatsina <dpats19@freeuni.edu.ge>
2024-06-15 12:43:12 +0400

    Add Getter anotation and remove getter methods

--- Theatre/src/main/java/com/example/theatre/api/dto/TheatreRequestDto.java
+++ Theatre/src/main/java/com/example/theatre/api/dto/TheatreRequestDto.java
@@ -2,26 +2,15 @@ package com.example.theatre.api.dto;
 
 import com.example.theatre.persistence.entity.Theatre;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class TheatreRequestDto {
     private String name;
     private String address;
 
     public TheatreRequestDto() {
-    }
-
-    public TheatreRequestDto(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public Theatre mapToEntity() {
