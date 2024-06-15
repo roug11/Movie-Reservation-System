fcab3fc0011199c0c0014cead705ee708f8ffb14
davidpatsina <dpats19@freeuni.edu.ge>
2024-06-15 12:44:57 +0400

    Delete unnesessar comment

--- Theatre/src/main/java/com/example/theatre/model/TheatreModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.theatre.model;
-
-public class TheatreModel {
-}
--- Theatre/src/main/java/com/example/theatre/persistence/entity/Theatre.java
+++ Theatre/src/main/java/com/example/theatre/persistence/entity/Theatre.java
@@ -6,7 +6,6 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "theater")
-//@SequenceGenerator(name = "theater_seq", sequenceName = "theater_seq", allocationSize = 1)
 public class Theatre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
